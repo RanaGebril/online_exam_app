61964f367acd66ceff7f5eea0bfe0ea08fbfbc48
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-08-05 12:16:05 +0300

    update auth api file

--- lib/features/auth/data/remote/auth_api_client.dart
+++ lib/features/auth/data/remote/auth_api_client.dart
@@ -1,32 +1,33 @@
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
+import '../../../../core/utils/constants/api_endpoint.dart';
 import '../models.dart';
 
 part 'auth_api_client.g.dart';
 
-@RestApi(baseUrl: 'https://exam.elevateegy.com/api/v1')
+@RestApi(baseUrl: ApiEndPionts.baseUrl)
 abstract class AuthApiClient {
   factory AuthApiClient(Dio dio, {String baseUrl}) = _AuthApiClient;
 
-  @POST('/auth/signin')
+  @POST(ApiEndPionts.signinEndPoint)
   Future<UserModel> login(@Body() Map<String, dynamic> body);
 
-  @POST('/auth/signup')
+  @POST(ApiEndPionts.signupEndPoint)
   Future<UserModel> signup(@Body() Map<String, dynamic> body);
   
-  @POST("/auth/forgotPassword")
+  @POST( ApiEndPionts.forgetpasswordEndPoint)
   Future<void> forgetpassword(@Body() Map<String, dynamic> body);
 
-  @POST("/auth/verifyResetCode")
+  @POST(ApiEndPionts.verifyresetcodeEndPoint)
   Future<void> verifyResetCode(@Body() Map<String, dynamic> body);
   
-  @POST("/auth/resetPassword")
+  @POST( ApiEndPionts.resetpasswordEndPoint)
   Future<void> resetPassword(@Body() Map<String, dynamic> body);
 
-  @POST("/auth/profileData")
+  @POST(ApiEndPionts.profiledataEndPoint)
   Future<void> profileData(@Body() Map<String, dynamic> body);
 
-  @POST("/auth/editProfile")
+  @POST(ApiEndPionts.editprofileEndPoint)
   Future<void> editProfile(
       @Body() Map<String, dynamic> body,
       @Header("Authorization") String token,
